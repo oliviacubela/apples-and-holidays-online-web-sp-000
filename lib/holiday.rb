6783ed1b8db8972d5d0044a5aaef63bebe53196f
Olivia Cubela <oliviacubela@gmail.com>
2020-06-17 16:59:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,7 +34,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  holiday_hash[:winter].values.to_a
+  holiday_hash[:winter].values
 end
 
 def all_supplies_in_holidays(holiday_hash)
