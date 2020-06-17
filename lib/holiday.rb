3304f3c12d221c34256c63d11534f67ca4d8fded
Olivia Cubela <oliviacubela@gmail.com>
2020-06-17 16:50:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -22,7 +22,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-holiday_hash[:winter].each { |holiday, supplies| supplies << supply }
+  holiday_hash[:winter].each { |holiday, supplies| supplies << supply }
 end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
