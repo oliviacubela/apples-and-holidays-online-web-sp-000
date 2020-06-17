7fc51d588959e2e5cb30eadc1e0adc43cda80a99
Olivia Cubela <oliviacubela@gmail.com>
2020-06-17 16:59:05 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -34,8 +34,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values.to_a
 end
 
 def all_supplies_in_holidays(holiday_hash)
